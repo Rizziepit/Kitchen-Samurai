da5bb8032380982fd31ebf93877801f7f98c16f8
Rizmari Versfeld <rizziepit@gmail.com>
2011-04-21 15:50:26 +0200

    Project

---
+++ Kitchen Samurai/Kitchen Samurai/Kitchen_SamuraiAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Kitchen_SamuraiAppDelegate.h
+//  Kitchen Samurai
+//
+//  Created by Rizmari Versfeld on 2011/04/21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Kitchen_SamuraiAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
